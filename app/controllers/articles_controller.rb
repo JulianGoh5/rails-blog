0746425052ced2b7d480669d7251a0de83ed9b25
Julian Goh <julian.goh@clearcalcs.com>
2022-06-30 16:16:24 +1000

    Overcome error render bug

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,26 +1,39 @@
 # frozen_string_literal: true
 
+# Notes
+# - index, create, new are all related to the Controller#Action as described after exec. the command 'bin/rails routes'
+# - standard CRUD actions: index, show, new , edit, create, update and destroy
+# - somehow the '@article' convention allows the controller to link into the views folder - '@foobar' is an instance variable of self, instance variables belong to a certain object
+
+
 class ArticlesController < ApplicationController
-  def index
-    @articles = Article.all
+  def index #URI /welcome/index(.:format)
+    @articles = Article.all # Where does articles come from? Presumably the OG 'bin/rails routes'
   end
 
-  def show
+  def show #URI /articles/:id(.:format)
     @article = Article.find(params[:id])
   end
 
-  def new; end
+  def new # URI /articles/new
+    @article = Article.new
+   end 
 
   def create
-    # render plain: params[:article].inspect
-    @article = Article.new(article_params)
+    @article = Article.new(article_params) #Initialises model 'Article' with params[:article]
 
-    @article.save
-    redirect_to @article
+    if  @article.save
+      redirect_to @article
+    else 
+      render 'new', status: 422
+    end
   end
 
   private
-    def article_params 
-      params.require(:article).permit(:title, :text)
-    end
+
+  def article_params
+    params.require(:article).permit(:title, :text)
   end
+end
+
+
