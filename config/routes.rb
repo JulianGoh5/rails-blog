456d0734c90f913e5cbd24a157f0967d1e3a9e8b
Julian Goh <julian.goh@clearcalcs.com>
2022-07-04 11:09:39 +1000

    Active Record, SQL, associating models

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,9 @@
 Rails.application.routes.draw do
   get 'welcome/index'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-  resources :articles # Establishes all the usual routing for '@articles'
+  resources :articles do # Establishes all the usual routing for '@articles'
+    resources :comments # Note how comments is nested, to signify the hierarchical relationship between articles and comments
+  end
   # Defines the root path route ("/")
   # root "articles#index"
   root 'welcome#index'
